09020fc516efbd2f18a027078c39ba687e65bc46
Mino Zang <minoscc+dev@gmail.com>
2021-02-26 13:31:13 +0800

    init

---
+++ index.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+const dotenv = require('dotenv');
+const dotenvExpand = require('dotenv-expand');
+
+function loadEnvFiles(options) {
+	const files = Array.isArray(options.filePath)
+		? options.filePath
+		: [options.filePath || path.resolve(process.cwd(), '.env')];
+
+	let config = {};
+	for (const file of files) {
+		if (fs.existsSync(file)) {
+			config = Object.assign(config, dotenv.parse(fs.readFileSync(file)));
+			config = dotenvExpand({parsed: config}).parsed || config;
+		}
+	}
+	return config;
+}
+
+function config(options = {}) {
+	const opts = Object.assign(
+		{
+			ignoreVars: false,
+		},
+		options
+	);
+
+	if (!opts.filePath) {
+		opts.filePath = [
+			'.env',
+			`.env.${process.env.NODE_ENV || 'development'}`,
+			`.env.${process.env.NODE_ENV || 'development'}.local`,
+		];
+	}
+	let config = loadEnvFiles(opts);
+
+	if (opts.ignoreVars) {
+		config = {
+			...config,
+			...process.env,
+		};
+	}
+
+	const keys = Object.keys(config).filter((key) => !(key in process.env));
+	keys.forEach((key) => (process.env[key] = config[key]));
+
+	return config;
+}
+
+module.exports = config;
